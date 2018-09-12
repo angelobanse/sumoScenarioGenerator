57a2c0b85a318a8e88fc413bfb4f64f93bd8c326
angelobanse <angelo_22@outlook.com>
2018-09-12 11:36:54 +0200

    Rearranging JavaScript code, the site loads faster

--- js/scenario-generator.js
+++ js/scenario-generator.js
@@ -1,3 +1,61 @@
+
+// Bootstrap components
+$(document).ready(function(){
+	$('[data-toggle="tooltip"]').tooltip();
+	$('[data-toggle="popover"]').popover(); 
+  });
+
+
+function openSideMenu(){
+  document.getElementById("side-menu").style.width="350px";
+  document.getElementById("btn-start").style.display="none";
+  document.getElementById("searchstring").style.display="none";
+  document.getElementById("searchbtn").style.display="none";
+  document.getElementById("btn-cur-location").style.display="none";
+}
+
+function closeSideMenu(){
+  document.getElementById("side-menu").style.width="0";
+  document.getElementById("btn-start").style.display="block";
+  document.getElementById("searchstring").style.display="block";
+  document.getElementById("searchbtn").style.display="block";
+  document.getElementById("btn-cur-location").style.display="block";
+  $('#collapseSelectedArea').text('No area selected');
+}
+
+// close SideMenu when pressing <esc> key
+$( document ).on( 'keydown', function ( e ) {
+  if ( e.keyCode === 27 ) {
+	  closeSideMenu();
+  }
+});
+
+
+// ============================== MAP ==============================
+
+var map = new ol.Map({
+	target: 'map',
+	layers: [
+	  new ol.layer.Tile({
+		source: new ol.source.OSM()
+	  })
+	],
+	view: new ol.View({
+	  center: ol.proj.fromLonLat([13.52822,52.4283]),
+	  zoom: 16
+	})
+  });
+
+  var mousePosition = new ol.control.ScaleLine({
+  units: 'metric',
+  minWidth: 100
+});
+map.addControl(mousePosition);
+
+
+
+
+// Get current location
 $('#btn-cur-location').on('click', function(event) {
 	if(!navigator.geolocation) return;
         else navigator.geolocation.getCurrentPosition(function(position){
@@ -5,6 +63,8 @@ $('#btn-cur-location').on('click', function(event) {
         });
     });
 
+
+// Search
 function search() {
     var searchstring = $('#searchstring').val();
 
@@ -27,6 +87,8 @@ function search() {
 	      });
 }
 
+
+// Go to selected search result
 function chooseAddr(lat, lng) {
     $('#searchresults').empty();
     var newlocation = ol.proj.transform([lng,lat], 'EPSG:4326', 'EPSG:3857');
@@ -37,8 +99,8 @@ function chooseAddr(lat, lng) {
 
 
 
-
-		function vehicleAdded(event, btn, vtype, defaultFactor, defaultCount){
+// When pressing the "Add" button
+function vehicleAdded(event, btn, vtype, defaultFactor, defaultCount){
 
 			// Are we handling a vehicle type or options button?
 			if (vtype != "options") {
@@ -69,17 +131,17 @@ function chooseAddr(lat, lng) {
 				$('#generate-'+vtype+'-panel').collapse('toggle');
 				
 				// prevent the submission of the form by clicking this button
-				event.preventDefault(); 
+				event.preventDefault();
+}
 
-		}
-			
-			  $('#btn-enable-cars').on('click',function(event){ vehicleAdded(event, this, 'car', 5, 12); });
-			  $('#btn-enable-trucks').on('click',function(event){ vehicleAdded(event, this, 'truck', 5, 8); });
-			  $('#btn-enable-bus').on('click',function(event){ vehicleAdded(event, this, 'bus', 5, 4); });
-			  $('#btn-enable-motorcycles').on('click',function(event){ vehicleAdded(event, this, 'motorcycle', 2, 4); });
-			  $('#btn-enable-bicycles').on('click',function(event){ vehicleAdded(event, this, 'bicycle', 2, 6); });
-			  $('#btn-enable-pedestrians').on('click',function(event){ vehicleAdded(event, this, 'pedestrian', 1, 10); });
-			  $('#btn-enable-trams').on('click',function(event){ vehicleAdded(event, this, 'tram', 20, 2); });
-			  $('#btn-enable-urbantrains').on('click',function(event){ vehicleAdded(event, this, 'urbantrain', 40, 2); });
-			  $('#btn-enable-trains').on('click',function(event){ vehicleAdded(event, this, 'train', 40, 2); });
-			  $('#btn-enable-ships').on('click',function(event){ vehicleAdded(event, this, 'ship', 40, 2); });
+// "Add" buttons
+$('#btn-enable-cars').on('click',function(event){ vehicleAdded(event, this, 'car', 5, 12); });
+$('#btn-enable-trucks').on('click',function(event){ vehicleAdded(event, this, 'truck', 5, 8); });
+$('#btn-enable-bus').on('click',function(event){ vehicleAdded(event, this, 'bus', 5, 4); });
+$('#btn-enable-motorcycles').on('click',function(event){ vehicleAdded(event, this, 'motorcycle', 2, 4); });
+$('#btn-enable-bicycles').on('click',function(event){ vehicleAdded(event, this, 'bicycle', 2, 6); });
+$('#btn-enable-pedestrians').on('click',function(event){ vehicleAdded(event, this, 'pedestrian', 1, 10); });
+$('#btn-enable-trams').on('click',function(event){ vehicleAdded(event, this, 'tram', 20, 2); });
+$('#btn-enable-urbantrains').on('click',function(event){ vehicleAdded(event, this, 'urbantrain', 40, 2); });
+$('#btn-enable-trains').on('click',function(event){ vehicleAdded(event, this, 'train', 40, 2); });
+$('#btn-enable-ships').on('click',function(event){ vehicleAdded(event, this, 'ship', 40, 2); });
