b9235b538e21f03919eb87d611effd13d495d3e2
angelobanse <angelo_22@outlook.com>
2018-10-09 13:38:08 +0200

    deleter.py now fully works (has been tested on a server)

--- deleter.py
+++ deleter.py
@@ -1,18 +1,20 @@
 # Olaf Angelo Banse - 2018
 
 # This script deletes .ZIP files older than 24hours
-# This script should be called by a CRON JOB
+# This script should be invoked every couple of hours by a CRON JOB
 import time
 import os
 
-epoch_now = time.time()
-directory = os.getcwd() + "/cgi-bin" # PATH where the ZIP files are being stored
-desired_time = epoch_now - (24 * 3600) # desired_time is epoch_now minus 24 hours (in seconds)
+epoch_now = time.time() # current time
 
+# PATH where the ZIP files are being stored
+directory = os.getcwd() + "/cgi-bin" # look carefully the path
+
+desired_time_limit = epoch_now - (24 * 3600) # desired_time_limit is epoch_now minus 24 hours (in seconds)
 os.chdir(directory)
 
 for zipFiles in os.listdir('.'):
-    if os.stat(os.path.join(directory, zipFiles)).st_mtime < desired_time and zipFiles.endswith(".zip"):
+    if os.stat(os.path.join(directory, zipFiles)).st_mtime < desired_time_limit and zipFiles.endswith(".zip"):
         os.remove(zipFiles)
     else:
         pass
